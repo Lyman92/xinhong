9226438594b3f6dc107b1355804338363d9de8a5
Lyman92 <m18396531029@163.com>
2018-01-03 23:47:23 +0800

    commit

--- util/urls.py
+++ util/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path(r'^(?P<method>\w*)/$', views.index, name='index'),
+    path('setFlash/', views.setFlash, name='setFlash'),
+    url(r'^(?P<method>\w*)/*$', views.index, name='index'),
 ]
--- util/views.py
+++ util/views.py
@@ -1,8 +1,39 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
+from datetime import date
+# from django.views.decorators.csrf import csrf_exempt
+
+from xinhong.slider.models import Slider
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'util/util.html')
+def index(request, method):
+    context = {}
+    template_name = ''
+    if method == '':
+        try:
+            slider = Slider.objects.all()[0]
+            context['title'] = slider.name
+        except:
+            context['title'] = ''
+        template_name = 'util/util.html'
+    return render(request, template_name, context)
+
+
+# @csrf_exempt
+def setFlash(request):
+    print('===================')
+    if request.method == 'POST':
+        try:
+            slider = Slider.objects.all()[0]
+            slider.name = request.POST['title']
+            slider.expiry_date = date.today()
+            slider.image = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1514999144106&di=bf3c6650e112c6a610181b908e7545eb&imgtype=0&src=http%3A%2F%2Fimg.800.cn%2F2015%2F01%2F23%2Fn_54c210a37894d.jpg'
+            slider.save()
+        except:
+            slider = Slider()
+            slider.name = request.POST['title']
+            slider.expiry_date = date.today()
+            slider.image = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1514999144106&di=bf3c6650e112c6a610181b908e7545eb&imgtype=0&src=http%3A%2F%2Fimg.800.cn%2F2015%2F01%2F23%2Fn_54c210a37894d.jpg'
+            slider.save()
+        return index(request,'')
